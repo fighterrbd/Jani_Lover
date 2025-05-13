4b86d2c0d9496355d5e9d82c41a6a24867459839
fighterrbd <fighterrbd@gmail.com>
2025-05-13 18:46:07 +0530

    Update thumbnails.py

--- NEXIOMUSIC/utils/thumbnails.py
+++ NEXIOMUSIC/utils/thumbnails.py
@@ -1,15 +1,17 @@
 import os
 import re
-import textwrap
 import random
 
 import aiofiles
 import aiohttp
-from PIL import Image, ImageDraw, ImageEnhance, ImageOps, ImageFilter, ImageFont
+
+from PIL import Image, ImageDraw, ImageEnhance
+from PIL import ImageFilter, ImageFont, ImageOps
+
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from NEXIOMUSIC import app
+from EsproMusic import app
 from config import YOUTUBE_IMG_URL
 
 
@@ -29,10 +31,6 @@ def clear(text):
         if len(title) + len(i) < 60:
             title += " " + i
     return title.strip()
-
-
-def get_random_color():
-    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), 255)
 
 
 async def get_thumb(videoid):
@@ -70,91 +68,63 @@ async def get_thumb(videoid):
                     await f.write(await resp.read())
                     await f.close()
 
+        
+        colors = ["white", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
+        border = random.choice(colors)
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        bg = Image.open(f"AloneX/assets/alone.png")
         image1 = changeImageSize(1280, 720, youtube)
-        image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(9))
-        enhancer = ImageEnhance.Brightness(background)
-        background = enhancer.enhance(0.5)
-
-        image3 = changeImageSize(1280, 720, bg)
-        image5 = image3.convert("RGBA")
-        Image.alpha_composite(background, image5).save(f"cache/temp{videoid}.png")
-
-        Xcenter = youtube.width / 2
-        Ycenter = youtube.height / 2
-        x1 = Xcenter - 250
-        y1 = Ycenter - 250
-        x2 = Xcenter + 250
-        y2 = Ycenter + 250
-
-        logo = youtube.crop((x1, y1, x2, y2))
-        logo.thumbnail((360, 360), Image.Resampling.LANCZOS)
-
-        border_size = 13
-        border_color = get_random_color()
-
-        bordered_logo = Image.new("RGBA", (logo.width + 2 * border_size, logo.height + 2 * border_size), (0, 0, 0, 0))
-        bordered_logo.paste(logo, (border_size, border_size))
-
-        draw = ImageDraw.Draw(bordered_logo)
-        draw.rectangle(
-            [(0, 0), (bordered_logo.width - 1, bordered_logo.height - 1)],
-            outline=border_color,
-            width=border_size
-        )
-
-        background.paste(bordered_logo, (750, 160), bordered_logo)
-        background.paste(image3, (0, 0), mask=image3)
-
-        draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("AloneX/assets/font2.ttf", 45)
-        font2 = ImageFont.truetype("AloneX/assets/font2.ttf", 70)
-        arial = ImageFont.truetype("AloneX/assets/font2.ttf", 30)
-        name_font = ImageFont.truetype("AloneX/assets/font.ttf", 30)
-        para = textwrap.wrap(title, width=30)
-        j = 0
-        draw.text((5, 5), f"AloneXMusic", fill="white", font=name_font)
-        for line in para:
-            if j == 1:
-                j += 1
-                draw.text(
-                    (60, 260),
-                    f"{line}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
-            if j == 0:
-                j += 1
-                draw.text(
-                    (60, 210),
-                    f"{line}",
-                    fill="white",
-                    stroke_width=1,
-                    stroke_fill="white",
-                    font=font,
-                )
+        bg_bright = ImageEnhance.Brightness(image1)
+        bg_logo = bg_bright.enhance(1.1)
+        bg_contra = ImageEnhance.Contrast(bg_logo)
+        bg_logo = bg_contra.enhance(1.1)
+        logox = ImageOps.expand(bg_logo, border=7, fill=f"{border}")
+        background = changeImageSize(1280, 720, logox)
+        # image2 = image1.convert("RGBA")
+        # background = image2.filter(filter=ImageFilter.BoxBlur(1))
+        #enhancer = ImageEnhance.Brightness(background)
+        #background = enhancer.enhance(0.9)
+        #draw = ImageDraw.Draw(background)
+        #arial = ImageFont.truetype("ISTKHAR_MUSIC/assets/font2.ttf", 30)
+        #font = ImageFont.truetype("ISTKHAR_MUSIC/assets/font.ttf", 30)
+        # draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
+        """
         draw.text(
-            (20, 675),
+            (1, 1),
             f"{channel} | {views[:23]}",
-            (255, 255, 255),
+            (1, 1, 1),
             font=arial,
         )
         draw.text(
-            (60, 400),
+            (1, 1),
+            clear(title),
+            (1, 1, 1),
+            font=font,
+        )
+        draw.line(
+            [(1, 1), (1, 1)],
+            fill="white",
+            width=1,
+            joint="curve",
+        )
+        draw.ellipse(
+            [(1, 1), (2, 1)],
+            outline="white",
+            fill="white",
+            width=1,
+        )
+        draw.text(
+            (1, 1),
             "00:00",
-            (255, 255, 255),
+            (1, 1, 1),
             font=arial,
         )
         draw.text(
-            (610, 400),
+            (1, 1),
             f"{duration[:23]}",
-            (255, 255, 255),
+            (1, 1, 1),
             font=arial,
         )
+        """
         try:
             os.remove(f"cache/thumb{videoid}.png")
         except:
@@ -164,3 +134,4 @@ async def get_thumb(videoid):
     except Exception as e:
         print(e)
         return YOUTUBE_IMG_URL
+    
